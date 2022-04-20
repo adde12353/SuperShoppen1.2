d6a1860f33e0c5688a0ea5e64038f942bfc1bd9a
Andreas Larsson <Andreas.per.larsson@gmail.com>
2022-04-20 10:17:05 +0200

    new update

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,9 @@
-import useFetch from "./components/Produktinfo/useFetchproduct";
 
 
 const Home = () => {
-    const {Data:Produkter} = useFetch("https://k4backend.osuka.dev/products/")
     
     return ( 
-        <div>{Produkter &&
-            Produkter.map(data => (
-                <div key={
-                    data.id
-                }>
-                <img src={data.image} alt="" />
-                <h3>{data.title}</h3>
-                </div>
-            ))}</div>
+        <div><h1>Välkommen till Supershoppen</h1></div>
      );
 }
  
